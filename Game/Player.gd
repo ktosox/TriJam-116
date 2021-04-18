afe9b6cb4b9982e97060845a76f76af144044abb
Ktosox <ktoso3@gmail.com>
2021-04-18 13:31:40 +0200

    made a player character in about 15 minutes

--- Game/Player.gd
+++ Game/Player.gd
@@ -4,12 +4,21 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var speed = 50.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		$AnimatedSprite.animation = "stand"
+	if event.is_action_released("ui_accept"):
+		$AnimatedSprite.animation = "walk"
 
 func _physics_process(delta):
-	move_and_slide(speed* Vector2(int(Input.is_action_pressed("ui_left") )-int(Input.is_action_just_pressed("ui_right")),0))
+	var moveVector = Vector2()
+	if !Input.is_action_pressed("ui_accept"):
+		moveVector.x = 8.0
+	move_and_slide(moveVector)
+
+	
